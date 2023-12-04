f12fb51ce41db7fd05b67547bc5ec1467b83dc8d
ragul28 <ragulh28@gmail.com>
2023-12-04 23:35:45 +0530

    Add workernode init

--- internal/worker.go
+++ internal/worker.go
@@ -4,9 +4,23 @@ import (
 	proto "github.com/ragul28/grpc-distributed-worker/proto"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 type WorkerNode struct {
 	conn *grpc.ClientConn        // grpc client connection
 	c    proto.NodeServiceClient // grpc client
+}
+
+func (n *WorkerNode) Init() (err error) {
+	// connect to controller node
+	n.conn, err = grpc.Dial(":50050", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return err
+	}
+
+	// grpc client
+	n.c = proto.NewNodeServiceClient(n.conn)
+
+	return nil
 }
