a35720f0ec8af94b82baa8d54b0e02102c2fcde7
ragul28 <ragulh28@gmail.com>
2023-12-04 23:36:53 +0530

    Add worker Start func & complete impl

--- internal/worker.go
+++ internal/worker.go
@@ -1,6 +1,11 @@
 package internal
 
 import (
+	"context"
+	"fmt"
+	"os/exec"
+	"strings"
+
 	proto "github.com/ragul28/grpc-distributed-worker/proto"
 
 	"google.golang.org/grpc"
@@ -23,4 +28,47 @@ func (n *WorkerNode) Init() (err error) {
 	n.c = proto.NewNodeServiceClient(n.conn)
 
 	return nil
+}
+
+func (n *WorkerNode) Start() {
+	// log
+	fmt.Println("worker node started")
+
+	// report status
+	_, _ = n.c.ReportStatus(context.Background(), &proto.Request{})
+
+	// assign task
+	stream, _ := n.c.AssignTask(context.Background(), &proto.Request{})
+	for {
+		// receive command from controller node
+		res, err := stream.Recv()
+		if err != nil {
+			return
+		}
+
+		// log command
+		fmt.Println("received command: ", res.Data)
+
+		// execute command
+		parts := strings.Split(res.Data, " ")
+		if err := exec.Command(parts[0], parts[1:]...).Run(); err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
+var workerNode *WorkerNode
+
+func GetWorkerNode() *WorkerNode {
+	if workerNode == nil {
+		// node
+		workerNode = &WorkerNode{}
+
+		// initialize node
+		if err := workerNode.Init(); err != nil {
+			panic(err)
+		}
+	}
+
+	return workerNode
 }
