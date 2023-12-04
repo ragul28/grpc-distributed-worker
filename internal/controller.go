f83e3e1fe1fe319d42f1534bb3fb5a6de76381bd
ragul28 <ragulh28@gmail.com>
2023-12-04 23:33:25 +0530

    Add Start func to run controller grpc & http server

--- internal/controller.go
+++ internal/controller.go
@@ -10,17 +10,44 @@ import (
 
 // Controller
 type ControllerNode struct {
-	api     *gin.Engine  // api server
-	ln      net.Listener // listener
-	svr     *grpc.Server // grpc server
-	nodeSvr *Server      // node service
+	api     *gin.Engine
+	ln      net.Listener
+	svr     *grpc.Server
+	nodeSvr *Server
 }
 
 func (n *ControllerNode) Init() (err error) {
-	// grpc server listener with port as 50051
 	n.ln, err = net.Listen("tcp", ":50050")
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+func (n *ControllerNode) Start() {
+	// start grpc server
+	go n.svr.Serve(n.ln)
+
+	// start api server
+	_ = n.api.Run(":9092")
+
+	// wait for exit
+	n.svr.Stop()
+}
+
+var controllerNode *ControllerNode
+
+// GetcontrollerNode returns the node instance
+func GetcontrollerNode() *ControllerNode {
+	if controllerNode == nil {
+		// node
+		controllerNode = &ControllerNode{}
+
+		// initialize node
+		if err := controllerNode.Init(); err != nil {
+			panic(err)
+		}
+	}
+
+	return controllerNode
 }
